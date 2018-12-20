1698d481c465e9c9bf74e968ce5900c88f183468
ragavaraaj <ragavaraaj@gmail.com>
2018-12-20 22:55:51 +0530

    removal of pass

--- user/connection.php
+++ user/connection.php
@@ -1,12 +1,15 @@
+
+<!-- this code was copied from a git hub user whos name i fogot  -->
+
 <?php
  
 Class Connection {
  
 private  $server = "mysql:host=localhost:3306;dbname=srilaksh_mathaji";
  
-private  $user = "srilaksh_mathaji";
+private  $user = "************";
  
-private  $pass = "mathajirootaccess";
+private  $pass = "************";
  
 private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
  
